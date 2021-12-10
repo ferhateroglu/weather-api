3ff31da6fe8d78989b76986e2c19c29cf80c35ef
Ferhat Eroglu <ferhateroglu1907@gmail.com>
2021-12-10 23:07:03 +0300

    sıcaklık ve rüzgar hızının api den çekilmesi

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const request = require('request');
-const URL = 'http://api.weatherapi.com/v1/current.json?key=2fab9888c0ee43d3a53190750211012&q=istanbul&aqi=no';
+const URL = 'http://api.weatherapi.com/v1/current.json?key=2fab9888c0ee43d3a53190750211012&q=Mersin&aqi=no';
 
 //({url:''},(response,error) =>{}); şeklinde parametre alır.
-request({url: URL }, (error,ser) => {
-    console.log(response);
+request({url: URL,json:true }, (error,response) => {
+    const {temp_c,wind_kph}=response.body.current;
+    console.log('Mersinde hava: '+temp_c+' derece ');
+    console.log('Rüzgar: '+ wind_kph);
 });
