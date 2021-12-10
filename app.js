f97a234d2b5742b214bf8d48505eb26cfab17b29
Ferhat Eroglu <ferhateroglu1907@gmail.com>
2021-12-11 01:22:24 +0300

    eklendi kullanıcıdan il bilgisi alma

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const request = require('request');
-const URL = 'http://api.weatherapi.com/v1/current.json?key=2fab9888c0ee43d3a53190750211012&q=Mersin&aqi=no';
+const il = process.argv[2];
+const URL = 'http://api.weatherapi.com/v1/current.json?key=2fab9888c0ee43d3a53190750211012&q='+il+'&aqi=no';
 
 //({url:''},(response,error) =>{}); şeklinde parametre alır.
 request({url: URL,json:true }, (error,response) => {
     const {temp_c,wind_kph}=response.body.current;
-    console.log('Mersinde hava: '+temp_c+' derece ');
-    console.log('Rüzgar: '+ wind_kph);
+    console.log(il+' ilinde hava: '+temp_c+' derece ');
+    console.log('Rüzgar: '+ wind_kph+'km/s');
 });
